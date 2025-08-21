738f99bfb657c2b1cf86d12fd343df3c29615b12
kolaches <djudosoej247hdh@outlook.com>
2025-08-21 12:21:46 +0300

    project 1.1

--- src/calc.js
+++ src/calc.js
@@ -2,5 +2,5 @@ export function add(a, b) {
   const x = Number(a);
   const y = Number(b);
   if (Number.isNaN(x) || Number.isNaN(y)) throw new Error("Некорректный ввод");
-  return x + y;
+  return x - y;
 }
